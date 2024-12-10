dd7b7c22fa3037e6b0d969725b6b0b21363ab1f1
anmamun0 <almamun20044@gmail.com>
2024-12-10 21:17:44 +0600

    file_Uploaded

--- Week_2/MODULE_4/forms_project/first_app/views.py
+++ Week_2/MODULE_4/forms_project/first_app/views.py
@@ -14,12 +14,18 @@ def contact(request):
 def submit_form(request): 
     return render(request,'form.html')
 def DjangoForm(request):
-    form = contactForm(request.POST) 
-
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request,'django_form.html',{'form':form})
+    if request.method == "POST":
+        form = contactForm(request.POST,request.FILES) 
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/'+ file.name,'wb+') as destination:
+                for chunck in file.chunks():
+                    destination.write(chunck)
+            print(form.cleaned_data)
+            return render(request,'django_form.html',{'form':form})
  
-
+    else:
+        form = contactForm()
+    return render(request,'django_form.html',{'form':form})
 
 
